c76eba05682dba837d2f0605eda2a88dc0de813b
inksharedsolutions <webdev.inkshared@gmail.com>
2020-08-18 06:53:23 +0800

    changes-18-2020

--- src/components/book-link/book-link.js
+++ src/components/book-link/book-link.js
@@ -9,7 +9,7 @@ const BookLink = (props)=>{
         <>
             <section className="fullSection">
 
-                {!Object.entries(props.data.ebooks).length==0 && (
+                {!Object.entries(props.data.ebooks).length ==0 && (
 
                     <nav className={`${props.data.isNonfront ? 'booklinkBlocks' : 'frontLinks'}`} >
                         <span>{`${props.data.isNonfront?'Ebooks': ''}`}</span>
@@ -17,21 +17,18 @@ const BookLink = (props)=>{
                         <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.stratton}>Stratton Press</a></li>
                         <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.barnes}>Barnes & Noble</a></li>
                     </nav>
+                )}
 
-                )}      
-
-                {!Object.entries(props.data.paperback).length==0 && (
-
+                {!Object.entries(props.data.paperback).length == 0 && (
                     <nav className={`${props.data.isNonfront ? 'booklinkBlocks' : 'frontLinks'}`} >
                         <span>{`${props.data.isNonfront?'Purchase Paperback': ''}`}</span>
                         <li><a rel="noopener noreferrer" target="_blank" href={props.data.paperback.amazon}>Amazon</a></li>
                         <li><a rel="noopener noreferrer" target="_blank" href={props.data.paperback.barnes}>Barnes & Noble </a></li>
                         <li><a rel="noopener noreferrer" target="_blank" href={props.data.paperback.booksamillion}>Books A Million</a></li>
                     </nav>
-                    
                 )}
 
-                {!Object.entries(props.data.reviews).length==0 && (
+                {!Object.entries(props.data.reviews).length == 0 && (
 
                     <nav className={`${props.data.isNonfront ? 'booklinkBlocks' : 'frontLinks'}`}>
                         <span>Reviews</span>
